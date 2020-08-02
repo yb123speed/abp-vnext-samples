4ac73ab48230721bbef41f543cc4ef8238e6dd9e
yb123speed <904896209@qq.com>
2020-08-02 23:17:18 +0800

    add asp.net core sample with controllers

---
+++ basic-samples/BasicAspNetCoreSample/AppModule.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.AspNetCore;
+using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+
+namespace BasicAspNetCoreSample
+{
+    [DependsOn(typeof(AbpAspNetCoreModule))]
+    [DependsOn(typeof(AbpAutofacModule))]
+    public class AppModule:AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            context.Services.AddControllers();
+            base.ConfigureServices(context);
+        }
+
+        public override void OnApplicationInitialization(ApplicationInitializationContext context)
+        {
+            var app = context.GetApplicationBuilder();
+            var env = context.GetEnvironment();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});           
+            app.UseConfiguredEndpoints();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ basic-samples/BasicAspNetCoreSample/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace BasicAspNetCoreSample.Controllers
+{
+    [Route("api/home")]
+    public class HomeController : AbpController
+    {
+        [HttpGet]
+        [Route("index")]
+        public string Index()
+        {
+            return "Hello Abp !";
+        }
+    }
+}
